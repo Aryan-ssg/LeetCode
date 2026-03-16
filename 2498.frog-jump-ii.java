33d68a2d0df97dc813e3028130fed0109763ae8d
Aryan-ssg <901aryan@gmail.com>
2026-03-16 14:12:00 +0530

    chillcode

---
+++ 2498.frog-jump-ii.java
@@ -0,0 +1,14 @@
+/*
+ * @lc app=leetcode id=2498 lang=java
+ *
+ * [2498] Frog Jump II
+ */
+
+// @lc code=start
+class Solution {
+    public int maxJump(int[] stones) {
+        
+    }
+}
+// @lc code=end
+
