007f7068ec71108055db6e1a38e4e9faeab696dd
Aryan-ssg <901aryan@gmail.com>
2026-04-05 20:04:39 +0530

    rotate image using transpose

--- 48.rotate-image.java
+++ 48.rotate-image.java
@@ -7,29 +7,42 @@
 // @lc code=start
 class Solution {
     public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        outerRotate(matrix,n,0,0);
-        
+        int n = matrix.length;
+        // outerRotate(matrix,n,0,0);
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j < n; j++) {
+                int temp = matrix[i][j];
+                matrix[i][j] = matrix[j][i];
+                matrix[j][i] = temp;
+            }
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n/2;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[i][n-j-1];
+                matrix[i][n-j-1]=temp;
+            }
+        }
 
-        
     }
-    public void outerRotate(int[][] matrix,int n,int row,int col){
-        
-        if(n<=1){
+    
+
+    public void outerRotate(int[][] matrix, int n, int row, int col) {
+
+        if (n <= 1) {
             return;
         }
-        
-         for(int i=0;i<n-1;i++){
-            int temp=matrix[row][col+i];
-            matrix[row][col+i]=matrix[n-1-i+row][col];
-            matrix[n-1-i+row][col]=matrix[n-1+row][n-1-i+col];
-            matrix[n-1+row][n-1-i+col]=matrix[i+row][n-1+col];
-            matrix[i+row][n-1+col]=temp;
+
+        for (int i = 0; i < n - 1; i++) {
+            int temp = matrix[row][col + i];
+            matrix[row][col + i] = matrix[n - 1 - i + row][col];
+            matrix[n - 1 - i + row][col] = matrix[n - 1 + row][n - 1 - i + col];
+            matrix[n - 1 + row][n - 1 - i + col] = matrix[i + row][n - 1 + col];
+            matrix[i + row][n - 1 + col] = temp;
 
         }
-        outerRotate(matrix,n-2,row+1,col+1);
+        outerRotate(matrix, n - 2, row + 1, col + 1);
 
     }
 }
 // @lc code=end
-
