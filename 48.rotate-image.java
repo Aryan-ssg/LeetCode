9de630a5e32da7b8886c6dc6ac1926e9ee98c8fd
Aryan-ssg <901aryan@gmail.com>
2026-04-05 13:10:28 +0530

    rotate image by 90 (recursive approach)

---
+++ 48.rotate-image.java
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode id=48 lang=java
+ *
+ * [48] Rotate Image
+ */
+
+// @lc code=start
+class Solution {
+    public void rotate(int[][] matrix) {
+        int n=matrix.length;
+        outerRotate(matrix,n,0,0);
+        
+
+        
+    }
+    public void outerRotate(int[][] matrix,int n,int row,int col){
+        
+        if(n<=1){
+            return;
+        }
+        
+         for(int i=0;i<n-1;i++){
+            int temp=matrix[row][col+i];
+            matrix[row][col+i]=matrix[n-1-i+row][col];
+            matrix[n-1-i+row][col]=matrix[n-1+row][n-1-i+col];
+            matrix[n-1+row][n-1-i+col]=matrix[i+row][n-1+col];
+            matrix[i+row][n-1+col]=temp;
+
+        }
+        outerRotate(matrix,n-2,row+1,col+1);
+
+    }
+}
+// @lc code=end
+
