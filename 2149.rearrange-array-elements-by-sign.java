2b21f6dc1401631329e9f716fe1c5988c502874b
Aryan-ssg <901aryan@gmail.com>
2026-04-07 23:50:18 +0530

    rearrange array elements by sign (slightly optimized)

--- 2149.rearrange-array-elements-by-sign.java
+++ 2149.rearrange-array-elements-by-sign.java
@@ -10,38 +10,57 @@ import java.util.*;
 class Solution {
     public int[] rearrangeArray(int[] nums) {
         int n = nums.length;
-        List<Integer> pos = new ArrayList<>();
-        List<Integer> neg = new ArrayList<>();
+        // List<Integer> pos = new ArrayList<>();
+        // List<Integer> neg = new ArrayList<>();
 
-        for (int i = 0; i < n; i++) {
-            if (nums[i] > 0) {
-                pos.add(nums[i]);
-            } else {
-                neg.add(nums[i]);
-            }
-        }
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while (i < n / 2 || j < n / 2) {
-            if (i == 0 && j == 0) {
-                nums[k] = pos.get(0);
-                i++;
-
-            } else if (i != j) {
-                nums[k] = neg.get(j);
-                j++;
-
-            } else {
-                nums[k] = pos.get(i);
-                i++;
+        int[] arr=new int[n];
+        int j=0;
+        for(int i=0;i<n;i++){
+            if(nums[i]>0){
+                arr[j]=nums[i];
+                j+=2;
 
             }
-            k++;
-
         }
+        j=1;
+        for(int i=0;i<n;i++){
+            if(nums[i]<0){
+                arr[j]=nums[i];
+                j+=2;
 
-        return nums;
+            }
+        }
+        
+
+        // for (int i = 0; i < n; i++) {
+        //     if (nums[i] > 0) {
+        //         pos.add(nums[i]);
+        //     } else {
+        //         neg.add(nums[i]);
+        //     }
+        // }
+        // int i = 0;
+        // int j = 0;
+        // int k = 0;
+        // while (i < n / 2 || j < n / 2) {
+        //     if (i == 0 && j == 0) {
+        //         nums[k] = pos.get(0);
+        //         i++;
+
+        //     } else if (i != j) {
+        //         nums[k] = neg.get(j);
+        //         j++;
+
+        //     } else {
+        //         nums[k] = pos.get(i);
+        //         i++;
+
+        //     }
+        //     k++;
+
+        // }
+
+        return arr;
 
     }
 }
