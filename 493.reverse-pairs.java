e57d347fb8916d22f0ecd471b6671365a2251fb2
Aryan-ssg <901aryan@gmail.com>
2026-04-09 23:46:13 +0530

    reverse pairs (merge sort approach)

--- 493.reverse-pairs.java
+++ 493.reverse-pairs.java
@@ -7,18 +7,60 @@
 // @lc code=start
 class Solution {
     public int reversePairs(int[] nums) {
-        int n = nums.length;
-        int revpairs = 0;
-        for (int i = 0; i < n; i++) {
-            long numi = nums[i];
-            for (int j = i + 1; j < n; j++) {
-                if (numi > 2 * (long)nums[j]) {
-                    revpairs++;
-                }
+        // int n = nums.length;
+        // int revpairs = 0;
+        // for (int i = 0; i < n; i++) {
+        //     long numi = nums[i];
+        //     for (int j = i + 1; j < n; j++) {
+        //         if (numi > 2 * (long)nums[j]) {
+        //             revpairs++;
+        //         }
+        //     }
+        // }
+        return mergeSort(nums,0,nums.length-1);
+
+    }
+
+    private static int mergeSort(int[] nums,int low,int high){
+        if(low>=high){
+            return 0;
+        }
+        int mid=(high-low)/2+low;
+        int count=0;
+        count+=mergeSort(nums,low,mid);
+        count+=mergeSort(nums,mid+1,high);
+        count+=merge(nums,low,mid,high);
+        return count;
+
+    }
+
+    private static int merge(int[] nums,int low,int mid,int high){
+        int count=0;
+        int right=mid+1;
+        for(int i=low;i<=mid;i++){
+            while(right<=high &&(long)nums[i]>2*(long)nums[right]){
+                right++;
+            }
+            count+=(right-(mid+1));
+        }
+        int [] temp=new int[high-low+1];
+        int left=low;
+        right=mid+1;
+        int k=0;
+        while(left<=mid && right<=high){
+            if(nums[left]<=nums[right]){
+                temp[k++]=nums[left++];
+            }else{
+                temp[k++]=nums[right++];
             }
         }
-        return revpairs;
+        while(left<=mid)temp[k++]=nums[left++];
+        while(right<=high) temp[k++]=nums[right++];
 
+        for(int i=0;i<temp.length;i++){
+            nums[low+i]=temp[i];
+        }
+        return count;
     }
 }
 // @lc code=end
