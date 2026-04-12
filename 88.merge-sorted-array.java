98b920dfe1d0c3f5a5b18efeb821bad295a863c6
Aryan-ssg <901aryan@gmail.com>
2026-04-12 19:40:58 +0530

    Revision:Merge sorted array

--- 88.merge-sorted-array.java
+++ 88.merge-sorted-array.java
@@ -7,26 +7,46 @@
 // @lc code=start
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
+        // int arr1 = m - 1;
+        // int arr2 = n - 1;
+        // int insert = nums1.length - 1;
+
+        // while (arr1 >= 0 && arr2 >= 0) {
+        // if (nums1[arr1] <= nums2[arr2]) {
+        // nums1[insert] = nums2[arr2];
+        // arr2--;
+        // insert--;
+        // } else {
+        // nums1[insert] = nums1[arr1];
+        // arr1--;
+        // insert--;
+        // }
+        // }
+
+        // while (arr2 >= 0) {
+        // nums1[insert] = nums2[arr2];
+        // arr2--;
+        // insert--;
+        // }
+
         int arr1 = m - 1;
         int arr2 = n - 1;
-        int insert = nums1.length - 1;
+        int insert = m + n - 1;
 
-        while (arr1 >= 0 && arr2 >= 0) {
-            if (nums1[arr1] <= nums2[arr2]) {
-                nums1[insert] = nums2[arr2];
-                arr2--;
-                insert--;
-            } else {
+        while (insert >= 0) {
+
+            if (arr1 >= 0 && arr2 >= 0 && nums1[arr1] >= nums2[arr2]) {
                 nums1[insert] = nums1[arr1];
                 arr1--;
-                insert--;
+            } else if (arr2 >= 0 && arr1 >= 0 && nums1[arr1] < nums2[arr2]) {
+                nums1[insert] = nums2[arr2];
+                arr2--;
+            } else if(arr2>=0){
+                nums1[insert] = nums2[arr2];
+                arr2--;
             }
-        }
-
-        while (arr2 >= 0) {
-            nums1[insert] = nums2[arr2];
-            arr2--;
             insert--;
+
         }
 
         // if(arr1!=0){
