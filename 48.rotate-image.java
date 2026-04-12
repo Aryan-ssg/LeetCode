b190748600f229ce3adb856d6bfcc77f03950b33
Aryan-ssg <901aryan@gmail.com>
2026-04-12 15:49:54 +0530

    Revision-rotate image

--- 48.rotate-image.java
+++ 48.rotate-image.java
@@ -7,15 +7,34 @@
 // @lc code=start
 class Solution {
     public void rotate(int[][] matrix) {
-        int n = matrix.length;
-        // outerRotate(matrix,n,0,0);
-        for (int i = 0; i < n; i++) {
-            for (int j = i; j < n; j++) {
-                int temp = matrix[i][j];
-                matrix[i][j] = matrix[j][i];
-                matrix[j][i] = temp;
+        // int n = matrix.length;
+        // // outerRotate(matrix,n,0,0);
+        // for (int i = 0; i < n; i++) {
+        //     for (int j = i; j < n; j++) {
+        //         int temp = matrix[i][j];
+        //         matrix[i][j] = matrix[j][i];
+        //         matrix[j][i] = temp;
+        //     }
+        // }
+        // for(int i=0;i<n;i++){
+        //     for(int j=0;j<n/2;j++){
+        //         int temp=matrix[i][j];
+        //         matrix[i][j]=matrix[i][n-j-1];
+        //         matrix[i][n-j-1]=temp;
+        //     }
+        // }
+        // Transpose of matrix
+        int n=matrix.length;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int temp=matrix[i][j];
+                matrix[i][j]=matrix[j][i];
+                matrix[j][i]=temp;
+
             }
+        
         }
+        //reverse all rows
         for(int i=0;i<n;i++){
             for(int j=0;j<n/2;j++){
                 int temp=matrix[i][j];
@@ -24,8 +43,8 @@ class Solution {
             }
         }
 
+
     }
-    
 
     public void outerRotate(int[][] matrix, int n, int row, int col) {
 
