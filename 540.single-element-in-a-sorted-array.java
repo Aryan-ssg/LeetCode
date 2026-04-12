e5b384272aaa3f5439d6d41f4e12f30cf6b34c53
Aryan-ssg <901aryan@gmail.com>
2026-04-12 22:42:53 +0530

    Revision:single element in a sorted array

--- 540.single-element-in-a-sorted-array.java
+++ 540.single-element-in-a-sorted-array.java
@@ -7,48 +7,76 @@
 // @lc code=start
 class Solution {
     public int singleNonDuplicate(int[] nums) {
+        // return singleNonDuplicate1(nums);
+        return singleNonDuplicate2(nums);
+        
+
+
+
+
+    }
+     public int singleNonDuplicate2(int[] nums) {
         int low=0;
         int high=nums.length-1;
-        int single=-1;
         while(low<high){
-            int mid=low+(high-low)/2;
-            
 
-            if(nums[mid]==nums[mid-1]){
-                int leftele=(mid-1)-low;
-                int rightele=high-mid;
-                if(leftele%2!=0){
-                    high=mid-2;
-                }
-                else if(rightele%2!=0){
-                    low=mid+1;
-                }
-            }else if(nums[mid]==nums[mid+1]){
-                int leftele=mid-low;
-                int rightele=high-(mid+1);
-                 if(leftele%2!=0){
-                    high=mid-1;
-                }
-                else if(rightele%2!=0){
+            int mid=low+(high-low)/2;
+            if(mid%2==1)mid--;
+            if(mid%2==0){
+                if(nums[mid]!=nums[mid+1]){
+                    high=mid;
+
+                }else{
                     low=mid+2;
                 }
-
-
-            }else{
-                single=nums[mid];
-                break;
+                
             }
 
             
         }
-        if(single!=-1){
+        return nums[low];
+
+
+        
+
+    }
+
+    public int singleNonDuplicate1(int[] nums) {
+        int low = 0;
+        int high = nums.length - 1;
+        int single = -1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == nums[mid - 1]) {
+                int leftele = (mid - 1) - low;
+                int rightele = high - mid;
+                if (leftele % 2 != 0) {
+                    high = mid - 2;
+                } else if (rightele % 2 != 0) {
+                    low = mid + 1;
+                }
+            } else if (nums[mid] == nums[mid + 1]) {
+                int leftele = mid - low;
+                int rightele = high - (mid + 1);
+                if (leftele % 2 != 0) {
+                    high = mid - 1;
+                } else if (rightele % 2 != 0) {
+                    low = mid + 2;
+                }
+
+            } else {
+                single = nums[mid];
+                break;
+            }
+
+        }
+        if (single != -1) {
             return single;
-        }else{
+        } else {
             return nums[low];
         }
-        
-        
     }
+
 }
 // @lc code=end
-
