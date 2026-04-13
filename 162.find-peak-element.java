eaaf85ba9239da3c724c19a85a104d687b681175
Aryan-ssg <901aryan@gmail.com>
2026-04-13 23:13:04 +0530

    find peak element (optimized)

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -72,7 +72,19 @@ class Solution {
         // } else {
         //     return index;
         // }
-        
+
+        int low=0;
+        int high=n-1;
+        while(low<high){
+            int mid=low+(high-low)/2;
+
+            if(nums[mid]<nums[mid+1]){
+                low=mid+1;
+            }else{
+                high=mid;
+            }
+        }
+        return low;
         
 
     }
