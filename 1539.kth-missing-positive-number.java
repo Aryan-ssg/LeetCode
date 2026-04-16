02b60211fe1e169fad2b87eae3215b38c54eed6b
Aryan-ssg <901aryan@gmail.com>
2026-04-16 21:59:51 +0530

    Kth Missing Positive Number (Optimized)

--- 1539.kth-missing-positive-number.java
+++ 1539.kth-missing-positive-number.java
@@ -7,30 +7,47 @@
 // @lc code=start
 class Solution {
     public int findKthPositive(int[] arr, int k) {
-        int n=arr.length;
-        int num=1;
-        int count=k;
-        int i=0;
-        int ans=0;
-        while(i<n){
-            if(arr[i]==num){
-                i++;
-                num++;
+        // int n=arr.length;
+        // int num=1;
+        // int count=k;
+        // int i=0;
+        // int ans=0;
+        // while(i<n){
+        //     if(arr[i]==num){
+        //         i++;
+        //         num++;
 
-            }else{
-                count--;
-                if(count==0){
-                    ans=num;
-                    break;
-                }
-                num++;
-            }
+        //     }else{
+        //         count--;
+        //         if(count==0){
+        //             ans=num;
+        //             break;
+        //         }
+        //         num++;
+        //     }
             
+        // }
+        // if(count!=0){
+        //     ans=arr[n-1]+count;
+        // }
+        // return ans;
+        int n=arr.length;
+        int low=0;
+        int high=n-1;
+        int miss=0;
+        int mid=0;
+        while(low<=high){
+            mid=low+(high-low)/2;
+            miss=arr[mid]-(mid+1);
+
+            if(miss<k){
+                low=mid+1;
+            }else{
+                high=mid-1;
+            }
         }
-        if(count!=0){
-            ans=arr[n-1]+count;
-        }
-        return ans;
+        return k+high+1;
+
 
 
         
