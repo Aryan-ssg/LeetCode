83140802d0be84f20e14e76d25578eeecacdba88
Aryan-ssg <901aryan@gmail.com>
2026-04-18 16:44:19 +0530

    Revision-sqrt(x)

--- 69.sqrt-x.java
+++ 69.sqrt-x.java
@@ -7,18 +7,38 @@
 // @lc code=start
 class Solution {
     public int mySqrt(int x) {
+
+        // long low=0;
+        // long high=x;
+        // while(low<=high){
+        //     long mid=low+(high-low)/2;
+        //     if(mid*mid>x){
+        //         high=mid-1;
+        //     }else {
+        //         low=mid+1;
+        //     }
+        // }
+        // return (int)high;
+        
+
+
         long low=0;
         long high=x;
+
         while(low<=high){
             long mid=low+(high-low)/2;
+
             if(mid*mid>x){
                 high=mid-1;
-            }else {
+            }else{
                 low=mid+1;
             }
         }
         return (int)high;
-        
+
+
+
+
     }
 }
 // @lc code=end
