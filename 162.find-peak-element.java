5cd44252b690beaeafd36510ee7dee72c5575f37
Aryan-ssg <901aryan@gmail.com>
2026-04-18 16:31:47 +0530

    Revision-Find Peak element

--- 162.find-peak-element.java
+++ 162.find-peak-element.java
@@ -6,85 +6,74 @@
 
 // @lc code=start
 class Solution {
-    public int findPeakElement(int[] nums) {
+    public int bruteForce(int[] nums) {
+        int index = 0;
         int n = nums.length;
-        //BRUTE FORCE
-        // int index = 0;
-        // if (n == 1) {
-        // return 0;
-        // }
-        // for (int i = 0; i < n; i++) {
-        // if (i == 0 && nums[i] > nums[i + 1]) {
+        if (n == 1) {
+            return 0;
+        }
+        for (int i = 0; i < n; i++) {
+            if (i == 0 && nums[i] > nums[i + 1]) {
 
-        // index=0;
-        // break;
+                index = 0;
+                break;
 
-        // }
-        // if (i == n - 1 && nums[i] > nums[i - 1]) {
+            }
+            if (i == n - 1 && nums[i] > nums[i - 1]) {
 
-        // index=n-1;
-        // break;
+                index = n - 1;
+                break;
 
-        // }
-        // if(nums[i]>nums[i+1] && nums[i]>nums[i-1]){
-        // index=i;
-        // break;
-        // }
+            }
+            if (nums[i] > nums[i + 1] && nums[i] > nums[i - 1]) {
+                index = i;
+                break;
+            }
 
-        // }
-        // return index;
-        // int index = 0;
-        // int low = 0;
-        // int high = n - 1;
-        // if (n == 1) {
-        //     return 0;
-        // }
-        // if (n == 2) {
-        //     if (nums[0] > nums[1]) {
-        //         return 0;
-        //     } else {
-        //         return 1;
-        //     }
-        // }
-        // while (low < high) {
-        //     int mid = low + (high - low) / 2;
-        //     if((high-low)==1){
-        //         if(nums[low]>nums[high]){
-        //             index=low;
-        //             break;
-        //         }else{
-        //             index=high;
-        //             break;
-        //         }
-        //     }
-        //     if (nums[mid] > nums[mid + 1] && nums[mid] > nums[mid - 1]) {
-        //         index = mid;
-        //         break;
+        }
+        return index;
 
-        //     } else if (nums[mid] < nums[mid + 1]) {
-        //         low = mid + 1;
-        //     } else {
-        //         high = mid - 1;
-        //     }
-        // }
-        // if (high == low) {
-        //     return low;
-        // } else {
-        //     return index;
-        // }
+    }
 
-        int low=0;
-        int high=n-1;
-        while(low<high){
-            int mid=low+(high-low)/2;
+    public int optimized(int[] nums) {
+        int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+        while (low < high) {
+            int mid = low + (high - low) / 2;
 
-            if(nums[mid]<nums[mid+1]){
-                low=mid+1;
-            }else{
-                high=mid;
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
             }
         }
         return low;
+
+    }
+
+    public int revision(int[] nums) {
+         int n = nums.length;
+        int low = 0;
+        int high = n - 1;
+
+        while (low < high) {
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] < nums[mid + 1]) {
+                low = mid + 1;
+            } else {
+                high = mid;
+            }
+        }
+        return high;
+    }
+
+    public int findPeakElement(int[] nums) {
+       
+        // return bruteForce(nums);
+        // return optimized(nums);
+        return revision(nums);
         
 
     }
