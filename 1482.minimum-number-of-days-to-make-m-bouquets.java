7194873cbbfa3b18c86506c31d7b478171526247
Aryan-ssg <901aryan@gmail.com>
2026-04-18 18:28:58 +0530

    Revision-minimum no. of days to make m bouquets

--- 1482.minimum-number-of-days-to-make-m-bouquets.java
+++ 1482.minimum-number-of-days-to-make-m-bouquets.java
@@ -6,48 +6,98 @@
 
 // @lc code=start
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        int n = bloomDay.length;
-        int max = 0;
-        int min = Integer.MAX_VALUE;
+    public int revision(int[] bloomDay,int m,int k){
+       
+        int n=bloomDay.length;
+
         if(((long)m*(long)k)>n){
             return -1;
         }
-        for (int i = 0; i < n; i++) {
-            if (bloomDay[i] > max) {
-                max = bloomDay[i];
-            } else if (bloomDay[i] < min) {
-                min = bloomDay[i];
+        int low=Integer.MAX_VALUE;
+        int high=0;
+        for(int i=0;i<n;i++){
+            if(bloomDay[i]>high){
+                high=bloomDay[i];
+            }
+            if(bloomDay[i]<low){
+                low=bloomDay[i];
             }
         }
 
-        int low = min;
-        int high = max;
-        while(low<=high){
+        while(low<high){
             int mid=low+(high-low)/2;
-            
-            int alt=0;
-            int Bouque=0;
+
+            int bouque=0;
+            int adj=0;
             for(int i=0;i<n;i++){
                 if(bloomDay[i]<=mid){
-                    alt++;
-                    if(alt==k){
-                        Bouque++;
-                        alt=0;
+                    adj++;
+                    if(adj==k){
+                        bouque++;
+                        adj=0;
                     }
 
                 }else{
-                    alt=0;
+                    adj=0;
                 }
             }
+            if(bouque>=m){
+                high=mid;
 
-            if(Bouque>=m){
-                high=mid-1;
             }else{
                 low=mid+1;
             }
+
         }
         return low;
+
+    }
+    public int minDays(int[] bloomDay, int m, int k) {
+
+
+        return revision(bloomDay,m,k);
+
+        // int n = bloomDay.length;
+        // int max = 0;
+        // int min = Integer.MAX_VALUE;
+        // if(((long)m*(long)k)>n){
+        //     return -1;
+        // }
+        // for (int i = 0; i < n; i++) {
+        //     if (bloomDay[i] > max) {
+        //         max = bloomDay[i];
+        //     } else if (bloomDay[i] < min) {
+        //         min = bloomDay[i];
+        //     }
+        // }
+
+        // int low = min;
+        // int high = max;
+        // while(low<=high){
+        //     int mid=low+(high-low)/2;
+            
+        //     int alt=0;
+        //     int Bouque=0;
+        //     for(int i=0;i<n;i++){
+        //         if(bloomDay[i]<=mid){
+        //             alt++;
+        //             if(alt==k){
+        //                 Bouque++;
+        //                 alt=0;
+        //             }
+
+        //         }else{
+        //             alt=0;
+        //         }
+        //     }
+
+        //     if(Bouque>=m){
+        //         high=mid-1;
+        //     }else{
+        //         low=mid+1;
+        //     }
+        // }
+        // return low;
     }
 }
 // @lc code=end
