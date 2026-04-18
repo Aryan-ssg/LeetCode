87142ba495e775dc7796d7edb59679e50838b0e0
Aryan-ssg <901aryan@gmail.com>
2026-04-18 21:17:20 +0530

    Revision-split array largest sum

--- 410.split-array-largest-sum.java
+++ 410.split-array-largest-sum.java
@@ -6,7 +6,7 @@
 
 // @lc code=start
 class Solution {
-    public int splitArray(int[] nums, int k) {
+    public int revision(int[] nums, int k) {
         int n = nums.length;
         int low = 0;
         int high = 0;
@@ -16,29 +16,68 @@ class Solution {
             }
             high += nums[i];
         }
+
         while (low < high) {
             int mid = low + (high - low) / 2;
+
+            int splits = 1;
             int sum = 0;
-            int splits=1;
             for (int i = 0; i < n; i++) {
                 sum += nums[i];
                 if (sum > mid) {
                     splits++;
                     sum = nums[i];
                 }
-                if(splits>k){
-                    break;
-                }
+                
 
             }
-            if(splits<=k){
-                high=mid;
-            }else{
+
+            if(splits>k){
                 low=mid+1;
+            }else{
+                high=mid;
             }
 
         }
         return low;
+    }
+
+    public int splitArray(int[] nums, int k) {
+
+        return revision(nums, k);
+
+        // int n = nums.length;
+        // int low = 0;
+        // int high = 0;
+        // for (int i = 0; i < n; i++) {
+        // if (nums[i] > low) {
+        // low = nums[i];
+        // }
+        // high += nums[i];
+        // }
+        // while (low < high) {
+        // int mid = low + (high - low) / 2;
+        // int sum = 0;
+        // int splits=1;
+        // for (int i = 0; i < n; i++) {
+        // sum += nums[i];
+        // if (sum > mid) {
+        // splits++;
+        // sum = nums[i];
+        // }
+        // if(splits>k){
+        // break;
+        // }
+
+        // }
+        // if(splits<=k){
+        // high=mid;
+        // }else{
+        // low=mid+1;
+        // }
+
+        // }
+        // return low;
 
     }
 }
