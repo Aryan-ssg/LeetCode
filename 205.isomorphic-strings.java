841b0a3b3d835bde389f57e361d26d3c23e414fb
Aryan-ssg <901aryan@gmail.com>
2026-05-01 16:33:17 +0530

    isomorphic strings

---
+++ 205.isomorphic-strings.java
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode id=205 lang=java
+ *
+ * [205] Isomorphic Strings
+ */
+
+// @lc code=start
+import java.util.*;
+class Solution {
+    public boolean isIsomorphic(String s, String t) {
+        Map<Character,Character> map=new HashMap<>();
+
+        for(int i=0;i<s.length();i++){
+            if(map.containsKey(s.charAt(i))){
+                if(t.charAt(i)!=map.get(s.charAt(i))){
+                    return false;
+                }
+            }else{
+                
+                if(map.containsValue(t.charAt(i))){
+                    return false;
+                }
+                map.put(s.charAt(i), t.charAt(i));
+            }
+
+        }
+        return true;
+        
+    }
+}
+// @lc code=end
+
