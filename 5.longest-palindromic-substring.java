8e7bf7e8281aa1f55324bfd0712873339fb6e719
Aryan-ssg <901aryan@gmail.com>
2026-05-03 18:28:01 +0530

    longest palindrome sybstring

--- 5.longest-palindromic-substring.java
+++ 5.longest-palindromic-substring.java
@@ -8,37 +8,36 @@
 class Solution {
     public String longestPalindrome(String s) {
 
-        int n=s.length();
-        String longest= s.charAt(0)+"";
-        String curr="";
-        for(int i=0;i<n;i++){
-            int high=i+1;
-            int low=i-1;
-            while(high<n && low>=0 && s.charAt(high)==s.charAt(low)){
-                curr=s.substring(low,high+1);
+        int n = s.length();
+        String longest = s.charAt(0) + "";
+        String curr = "";
+        for (int i = 0; i < n; i++) {
+            int high = i + 1;
+            int low = i - 1;
+            while (high < n && low >= 0 && s.charAt(high) == s.charAt(low)) {
+                curr = s.substring(low, high + 1);
                 high++;
                 low--;
 
             }
-            if(curr.length()>longest.length()){
-                longest=curr;
+            if (curr.length() > longest.length()) {
+                longest = curr;
             }
-             high=i+1;
-             low=i;
-             while(high<n && low>=0 && s.charAt(high)==s.charAt(low)  ){
-                curr=s.substring(low,high+1);
+            high = i + 1;
+            low = i;
+            while (high < n && low >= 0 && s.charAt(high) == s.charAt(low)) {
+                curr = s.substring(low, high + 1);
                 high++;
                 low--;
 
             }
-            if(curr.length()>longest.length()){
-                longest=curr;
+            if (curr.length() > longest.length()) {
+                longest = curr;
             }
         }
-        
+
         return longest;
-        
+
     }
 }
 // @lc code=end
-
